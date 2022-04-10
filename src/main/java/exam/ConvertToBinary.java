083bbee54d9de2b935e1ca57dc88be456b94a1b8
Rushikesh Modhe <97840054+RushikeshModhe@users.noreply.github.com>
2022-04-10 15:10:45 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -2,8 +2,27 @@ package exam;
 
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+  public String toBinary(final int n1) {
+            int n=n1;
+           
+          String binary="";
+            while (n>=0)
+            {
+            	   int rem = n % 2;
+            	 if(n==0)
+            	 {
+            	     break;
+            	 }
+        	     if(rem == 0)
+        	     {
+        	         binary=binary+0;
+        	     }
+    	        else
+    	         {
+    	              binary=binary+1;
+    	         }
+    	     	        n = n/2;
+            }
+      return new StringBuffer(binary).reverse().toString();
   }
 }
